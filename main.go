0cc9a4d2ba641d071b080244f366d6569b16ad36
Jean-Claude Wippler <jc@wippler.nl>
2014-03-23 23:22:50 +0100

    better cmdline help

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// HouseMon: process config, env vars, cmdline args, then start up as needed.
 package main
 
 import (
@@ -10,54 +11,39 @@ import (
 	jeebus "github.com/jcw/jeebus/gadgets"
 )
 
-var VERSION = "0.9.0" // can be adjusted by goxc
+var VERSION = "0.9.0" // can be adjusted by goxc at link time
+var BUILD_DATE = ""   // can be adjusted by goxc at link time
 
+// defaults can also be overridden through environment variables
 const defaults = `
-# can be overridden with environment variables
 APP_DIR = ./app
 BASE_DIR = ./base
 DATA_DIR = ./data
 PORT = 5561
 `
 
-var config = jeebus.LoadConfig(defaults, "./config.txt")
-
-func init() {
-	flow.Registry["info"] = func() flow.Circuitry { return &infoCmd{} }
-	jeebus.Help["info"] = `Show some basic information about HouseMon.`
-}
-
-type infoCmd struct{ flow.Gadget }
-
-func (g *infoCmd) Run() {
-	fmt.Printf("HouseMon %s + JeeBus %s + Flow %s\n\n",
-		VERSION, jeebus.Version, flow.Version)
-	flow.PrintRegistry()
-	fmt.Println("\nUse 'help' for a list of commands or '-h' for a list of options.")
-}
-
 func main() {
-	flag.Parse()
+	flag.Parse() // required, to set up the proper glog configuration
+	flow.LoadConfig(defaults, "./config.txt")
 
 	if flag.NArg() > 0 {
 		if factory, ok := flow.Registry[flag.Arg(0)]; ok {
 			factory().Run()
 			return
 		}
-		fmt.Fprintln(os.Stderr, "Unknown command:", flag.Arg(0), "(try 'help')")
-		fmt.Fprintln(os.Stderr, "See http://jeelabs.net/projects/housemon/wiki")
+		fmt.Fprintln(os.Stderr, "Unknown command:", flag.Arg(0))
 		os.Exit(1)
 	}
 
-	fmt.Printf("Starting webserver for http://localhost:%s/\n", config["PORT"])
+	fmt.Printf("Starting webserver for http://localhost:%s/\n", flow.Config["PORT"])
 
 	// show intro page via a static webserver if the main app dir is absent
-	fd, err := os.Open(config["APP_DIR"])
+	fd, err := os.Open(flow.Config["APP_DIR"])
 	if err != nil {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte(introPage))
 		})
-		panic(http.ListenAndServe(":"+config["PORT"], nil))
+		panic(http.ListenAndServe(":"+flow.Config["PORT"], nil))
 	}
 	fd.Close()
 
@@ -73,13 +59,14 @@ func setupDatabase() {
 	c.Add("sink", "Sink")
 	c.Connect("db.Out", "sink.In", 0)
 	c.Connect("db.Mods", "sink.In", 0)
-	c.Feed("db.Name", config["DATA_DIR"])
+	c.Feed("db.Name", flow.Config["DATA_DIR"])
 	c.Feed("db.In", flow.Tag{"<clear>", "/config/"})
-	for k, v := range config {
+	for k, v := range flow.Config {
 		c.Feed("db.In", flow.Tag{"/config/" + k, v})
 	}
-	c.Feed("db.In", flow.Tag{"/config/app", "HouseMon"})
+	c.Feed("db.In", flow.Tag{"/config/appName", "HouseMon"})
 	c.Feed("db.In", flow.Tag{"/config/version", VERSION})
+	c.Feed("db.In", flow.Tag{"/config/buildDate", BUILD_DATE})
 	c.Run()
 }
 
@@ -88,10 +75,10 @@ func setupWebserver() {
 	c := flow.NewCircuit()
 	c.Add("http", "HTTPServer")
 	c.Add("forever", "Forever") // run forever
-	c.Feed("http.Handlers", flow.Tag{"/", config["APP_DIR"]})
-	c.Feed("http.Handlers", flow.Tag{"/base", config["BASE_DIR"]})
+	c.Feed("http.Handlers", flow.Tag{"/", flow.Config["APP_DIR"]})
+	c.Feed("http.Handlers", flow.Tag{"/base", flow.Config["BASE_DIR"]})
 	c.Feed("http.Handlers", flow.Tag{"/ws", "<websocket>"})
-	c.Feed("http.Start", config["PORT"])
+	c.Feed("http.Start", flow.Config["PORT"])
 	c.Run()
 }
 
@@ -118,3 +105,32 @@ const introPage = `<!DOCTYPE html>
     </script>
   </body>
 </html>`
+
+// Show some additional application information when printing usage info.
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "HouseMon (%s) + JeeBus (%s) + Flow (%s) %s\n",
+			VERSION, jeebus.Version, flow.Version, BUILD_DATE)
+		fmt.Fprintln(os.Stderr, "\nDebug options:")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nTry 'help' for more info, or visit %s\n",
+			"http://jeelabs.net/projects/housemon/wiki")
+	}
+}
+
+// Here is an example of how to define a new gadget. This one has no input or
+// output ports, is registered using a lowercase name, and has a help entry.
+// It's only intended for use from the command line, i.e. "housemon info".
+
+func init() {
+	flow.Registry["info"] = func() flow.Circuitry { return &infoCmd{} }
+	jeebus.Help["info"] = `Show the list of registered gadgets and circuits.`
+}
+
+type infoCmd struct{ flow.Gadget }
+
+func (g *infoCmd) Run() {
+	fmt.Println("Registered gadgets and circuits:\n")
+	flow.PrintRegistry()
+	fmt.Println()
+}
