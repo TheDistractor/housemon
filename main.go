f6201cabec9849948de9454bf2e3b9ee76c04ba3
Jean-Claude Wippler <jc@wippler.nl>
2014-03-23 21:15:59 +0100

    add main startup logic to housemon app

--- main.go
+++ main.go
@@ -1,100 +1,98 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/jcw/flow"
-	_ "github.com/jcw/jeebus/gadgets"
+	jeebus "github.com/jcw/jeebus/gadgets"
 )
 
-var settings = map[string]string{
-	"appDir": "./app",
-	"baseDir": "./base",
-	"dataDir": "./data",
-	"port": "5561",
+var VERSION = "0.9.0" // can be adjusted by goxc
+
+const defaults = `
+    # can be overridden with environment variables
+    APP_DIR = ./app
+    BASE_DIR = ./base
+    DATA_DIR = ./data
+    PORT = 5561
+`
+
+var config = jeebus.LoadConfig(defaults, "./config.txt")
+
+func init() {
+	flow.Registry["info"] = func() flow.Circuitry { return &infoCmd{} }
+	jeebus.Help["info"] = `Show some basic information about HouseMon.`
+}
+
+type infoCmd struct{ flow.Gadget }
+
+func (g *infoCmd) Run() {
+	fmt.Printf("HouseMon %s + JeeBus %s + Flow %s\n\n",
+		VERSION, jeebus.Version, flow.Version)
+	flow.PrintRegistry()
+	fmt.Println("\nUse 'help' for a list of commands or '-h' for a list of options.")
 }
 
 func main() {
-	loadSettings("./settings.txt")
-	
-	fmt.Printf("Starting webserver at http://localhost:%s/\n", settings["port"])
-	
-	// show an intro page via a special webserver if the main app dir is absent
-	if !fileExists(settings["appDir"]) {
-		startIntroServer() // never returns
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		if factory, ok := flow.Registry[flag.Arg(0)]; ok {
+			factory().Run()
+			return
+		}
+		fmt.Fprintln(os.Stderr, "Unknown command:", flag.Arg(0), "(try 'help')")
+		fmt.Fprintln(os.Stderr, "See http://jeelabs.net/projects/housemon/wiki")
+		os.Exit(1)
 	}
 
+	fmt.Printf("Starting webserver for http://localhost:%s/\n", config["PORT"])
+
+	// show intro page via a static webserver if the main app dir is absent
+	fd, err := os.Open(config["APP_DIR"])
+	if err != nil {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(introPage))
+		})
+		panic(http.ListenAndServe(":"+config["PORT"], nil))
+	}
+	fd.Close()
+
+	// normal startup: save config info in database, then start the webserver
+	setupDatabase()
+	setupWebserver()
+}
+
+func setupDatabase() {
+	// database setup, save version and current config settings
 	c := flow.NewCircuit()
-	c.Add("http", "HTTPServer")
-	c.Add("forever", "Forever")
-	c.Feed("http.Handlers", flow.Tag{"/", settings["appDir"]})
-	c.Feed("http.Handlers", flow.Tag{"/base", settings["baseDir"]})
-	c.Feed("http.Handlers", flow.Tag{"/ws", "<websocket>"})
-	c.Feed("http.Start", settings["port"])
+	c.Add("db", "LevelDB")
+	c.Add("sink", "Sink")
+	c.Connect("db.Out", "sink.In", 0)
+	c.Connect("db.Mods", "sink.In", 0)
+	c.Feed("db.Name", config["DATA_DIR"])
+	c.Feed("db.In", flow.Tag{"<clear>", "/config/"})
+	for k, v := range config {
+		c.Feed("db.In", flow.Tag{"/config/" + k, v})
+	}
+	c.Feed("db.In", flow.Tag{"/config/app", "HouseMon"})
+	c.Feed("db.In", flow.Tag{"/config/version", VERSION})
 	c.Run()
 }
 
-// startIntroServer shows a single fixed page explaining what's going on
-func startIntroServer() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(introPage))
-	})
-	panic(http.ListenAndServe(":" + settings["port"], nil))
-}
-
-// fileExists returns true if the specified file or directory exists
-func fileExists(name string) bool {
-	if name != "" {
-		if fd, err := os.Open(name); err == nil {
-			fd.Close()
-			return true
-		}
-	}
-	return false
-}
-
-// loadSettings parses a settings file, if it exists, to configure some basic
-// application settings, such as where the app/ and data/ directories are.
-// For "fooBarDir", it also allows overriding via a "FOO_BAR_DIR" env variable.
-func loadSettings(filename string) {
-	fd, err := os.Open(filename)
-	if err != nil {
-		return
-	}
-	defer fd.Close()
-
-	scanner := bufio.NewScanner(fd)
-	for scanner.Scan() {
-		line := strings.Trim(scanner.Text(), " \t")
-		if line != "" && !strings.HasPrefix(line, "#") {
-			fields := strings.SplitN(line, "=", 2)
-			if len(fields) != 2 {
-				log.Fatalln("cannot parse settings:", scanner.Text())
-			}
-			key := strings.Trim(fields[0], " \t")
-			value := strings.Trim(fields[1], " \t")
-			env := os.Getenv(key)
-			if env != "" {
-				value = env
-			}
-			settings[capsToAllCaps(key)] = value
-		}
-	}
-}
-
-// capsToAllCaps converts "fooBarDir" to "FOO_BAR_DIR"
-func capsToAllCaps(s string) (result string) {
-	s = strings.ToLower(s)
-	t := strings.Split(s, "_")
-	for i, _ := range t {
-		result += strings.ToUpper(t[i][:1]) + t[i][1:]
-	}
-	return
+func setupWebserver() {
+	// webserver setup
+	c := flow.NewCircuit()
+	c.Add("http", "HTTPServer")
+	c.Add("forever", "Forever") // run forever
+	c.Feed("http.Handlers", flow.Tag{"/", config["APP_DIR"]})
+	c.Feed("http.Handlers", flow.Tag{"/base", config["BASE_DIR"]})
+	c.Feed("http.Handlers", flow.Tag{"/ws", "<websocket>"})
+	c.Feed("http.Start", config["PORT"])
+	c.Run()
 }
 
 // introPage contains the HTML shown when the application cannot start normally
